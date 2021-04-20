32f7a41a1ee0836fd705c4d968797e9d757af45e
PushkaryovVD <vadimka-pushkarev@mail.ru>
2021-04-20 15:26:37 +0600

    add all commit

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php
+$config=array(
+	'title'=> 'Мой блог',
+	'vk_url' =>'https://vk.com/id160504806',
+	'db'=> array(
+		'server'=>'localhost',
+		'username'=>'root',
+		'password'=>'admin123',
+		'name'=>'test_blog'
+	)
+);
+require "db.php";
